97600f3d00ab8ca10de1eac53d1fa64d9f6a7371
Vincent Ramdhanie <vramdhanie@gmail.com>
2014-11-27 23:08:15 -0400

    Initial commit of local repository

---
+++ SequentialSA/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  SequentialSA
+//
+//  Created by Vincent Ramdhanie on 11/27/14.
+//  Copyright (c) 2014 Vincent Ramdhanie. All rights reserved.
+//
+
+#include <stdio.h>
+
+void start();
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    start();
+    return 0;
+}
+
+void start(){
+    printf("In the Function\n");
+}
