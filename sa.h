268f2a58905b8528cdc17556388dd28848fa18d0
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-09-20 15:44:26 -0400

    Move constant declarations to header file

--- sa.h
+++ sa.h
@@ -1,6 +1,11 @@
 #ifndef FINAL_SEQUENTIALSA_SA_H_
 #define FINAL_SEQUENTIALSA_SA_H_
 
+#define TEMPERATURE 100 /*Initial temperature*/
+#define TEMPERATURE_DECREMENT 0.99 /*The amount by which teh temperature is decrement each iteration*/
+#define TEMPERATURE_FINAL 0.01 /*The coolest temperature, time to stop*/
+#define NUMBER_ITERATIONS 1000 /*Total number of iterations to execute before reducing temperature*/
+#define ITERATIONS_PER_FILE 5 /*Number of times to run each input file*/
 
 
 #endif  // FINAL_SEQUENTIALSA_SA_H_
